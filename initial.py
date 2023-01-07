870327bd15248baf41c9732ff2c321d1e466a587
ldjokic <lazar.m.djokic@gmail.com>
2023-01-07 16:10:19 +0100

    Changes the link to the website

--- initial.py
+++ initial.py
@@ -1,3 +1,4 @@
+import flask
 from bs4 import BeautifulSoup
 import requests
 
@@ -5,10 +6,10 @@ import requests
 
 # TODO
 # Search all pages based on keywords - category - price with descanding order of prices
-page = requests.get("https://novi.kupujemprodajem.com/kompjuteri-laptop-i-tablet/pretraga?keywords=thinkpad&categoryId=1221&hasPrice=yes&order=price%20desc&page=1")
+page = requests.get("https://novi.kupujemprodajem.com/pretraga?keywords=thinkpad&hasPrice=yes&order=price%20desc")
 soup = BeautifulSoup(page.content, 'html.parser')
 
 items_name = soup.find_all('div', attrs={'class':'AdItem_name__BppRQ'})
 item_prices = soup.find_all('div', attrs={'class':'AdItem_price__k0rQn'})
 
-print(type(items_name), type(item_prices))
+#print(type(items_name), type(item_prices))
