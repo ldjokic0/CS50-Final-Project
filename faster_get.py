9b953791d442eb6167ed0f4b5384bc4d6a715404
ldjokic <lazar.m.djokic@gmail.com>
2023-01-09 16:01:27 +0100

    Updates faster_get.py

--- faster_get.py
+++ faster_get.py
@@ -1,10 +1,7 @@
 import requests
-import requests_async
-import aiohttp
-import asyncio
 import time
+from concurrent.futures import ProcessPoolExecutor
 from bs4 import BeautifulSoup
-#from price_parser import Price
 
 def find_last_page(soup):
     # Classes are predifined by looking them up on the website
@@ -14,47 +11,49 @@ def find_last_page(soup):
 
     return int(last_page)
 
-async def get_item_data(urls):
+def fetch_url_data(pg_url):
+    try:
+        resp = requests.get(pg_url)
+    except Exception as e:
+        print(f"Error occured during fetch data from url{pg_url}")
+    else:
+        return resp.content
+        
+def get_all_url_data(url_list):
+    with ProcessPoolExecutor() as executor:
+        resp = executor.map(fetch_url_data, url_list)
+    return resp
+    
 
-    async with aiohttp.ClientSession() as session:
+if __name__=='__main__':
 
-        tasks = []
-        for url in urls:
-            task = asyncio.create_task(requests_async.get(url))
-            tasks.append(task)
+    link = "https://novi.kupujemprodajem.com/pretraga?keywords=thinkpad&hasPrice=yes&order=price%20desc&page=1"
 
-        responses = await asyncio.gather(*tasks)
+    page = requests.get(link)
+    soup = BeautifulSoup(page.content, 'html.parser')
+    last_page = find_last_page(soup)
+    # Make list of all urls
+    urls = [link[:-1] + str(i) for i in range(1, last_page + 1)]
+    all_data = {}
 
-        return responses
+    start_time = time.time()
+    responses = get_all_url_data(urls)
+    print(f'Fetch total {last_page} urls and process takes {time.time() - start_time} seconds')
 
-link = "https://novi.kupujemprodajem.com/pretraga?keywords=thinkpad&hasPrice=yes&order=price%20desc&page=1"
+    item_counter = 0
+    for response in responses:
 
-page = requests.get(link)
-soup = BeautifulSoup(page.content, 'html.parser')
-last_page = find_last_page(soup)
-# Make list of all urls
-urls = [link[:-1] + str(i) for i in range(1, last_page + 1)]
-all_data = {}
+        soup = BeautifulSoup(response, 'html.parser')
 
-start_time = time.time()
-responses = asyncio.run(get_item_data(urls))
-print("--- %s seconds ---" % (time.time() - start_time))
+        item_names = soup.find_all('div', attrs={'class':'AdItem_name__BppRQ'})
+        item_prices = soup.find_all('div', attrs={'class':'AdItem_price__k0rQn'})
 
+        get_item_names = [name.getText() for name in item_names]
+        get_item_prices = [price.getText() for price in item_prices]
 
-# Item counter is neccessary due to same item names occurence
-item_counter = 0
-for response in responses:
-    soup = BeautifulSoup(response.decode('utf-8'), 'html.parser')
+        for count, name in enumerate(get_item_names):
+            all_data[str(item_counter + count) + name] = get_item_prices[count]
 
-    item_names = soup.find_all('div', attrs={'class':'AdItem_name__BppRQ'})
-    item_prices = soup.find_all('div', attrs={'class':'AdItem_price__k0rQn'})
+        item_counter += len(get_item_names)
 
-    get_item_names = [name.getText() for name in item_names]
-    get_item_prices = [price.getText() for price in item_prices]
-
-    for count, name in enumerate(get_item_names):
-        all_data[str(item_counter + count) + name] = get_item_prices[count]
-
-    item_counter += len(get_item_names)
-
-print(len(all_data))
+    print(len(all_data))
