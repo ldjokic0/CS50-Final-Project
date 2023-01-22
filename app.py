351317997cd27ac750786262410d175664b1b05b
ldjokic <lazar.m.djokic@gmail.com>
2023-01-22 15:35:40 +0100

    Adds SQL database and register feature

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
 from flask import Flask, redirect, render_template, request, session
 from pull_data import kp_search
-
+from werkzeug.security import check_password_hash, generate_password_hash
+import sqlite3
 
 app = Flask(__name__)
 app.config["TEMPLATES_AUTO_RELOAD"] = True
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
+
+con = sqlite3.connect('SAH.db', check_same_thread=False)
+db = con.cursor()
 
 """ @app.after_request
 def after_request(response):
@@ -34,6 +38,23 @@ def index():
 
 @app.route("/register", methods=["GET", "POST"])
 def register():
+    print("dada")
+    if request.method == "POST":
+        
+        if not request.form.get("username"):
+            return render_template("register.html", warning = True)
+
+        # TODO: Add conditions for password (atleast 6 letters with alphanumeric and numeric characters), check if username already exists
+
+        username = request.form.get("username")
+        password = request.form.get("password")
+        hash_password = generate_password_hash(password)
+
+        db.execute("INSERT INTO users (username, hash_password) VALUES (?, ?)", (username, hash_password))
+        con.commit()
+
+        return render_template("home.html")
+
     return render_template("register.html")
 
 @app.route("/login", methods=["GET", "POST"])
