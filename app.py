67ec921e2ce59928555bb31b1ec9aa5d72c52390
ldjokic <lazar.m.djokic@gmail.com>
2023-02-01 13:39:21 +0100

    Changes to the style of the code

--- app.py
+++ app.py
@@ -14,15 +14,9 @@ app.secret_key = generate_password_hash("Not very secret key")
 con = sqlite3.connect('SAH.db', check_same_thread=False)
 db = con.cursor()
 
-""" @app.after_request
-def after_request(response):
-    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
-    response.headers["Expires"] = 0
-    response.headers["Pragma"] = "no-cache"
-    return response """
-
 # Define warning message when required field is not filled
 generic_warning_message = "Please fill out this field."
+
 
 @app.route("/", methods=["GET", "POST"])
 def index():
@@ -30,16 +24,15 @@ def index():
     if request.method == "POST":
         # Ensure user provides search word and selects website
         if not request.form.get("keyword"):
-            return render_template("home.html", warning = generic_warning_message, search_error = True)
+            return render_template("home.html", warning=generic_warning_message, search_error=True)
         elif not request.form.get("selected_website"):
-            return render_template("home.html", warning = "Please select website.", select_error = True)
+            return render_template("home.html", warning="Please select website.", select_error=True)
 
         keyword = request.form.get("keyword")
 
         # Utilizes function for searching items from kp (short for kupujemprodajem) website
         items, count = kp_search(keyword)
-        print(count)
-        
+
         if not items:
             # Inform user that search does not produce results
             flash(f"There are {count} results for '{keyword}'.")
@@ -63,10 +56,10 @@ def index():
         mean_price, median_price = round(mean(prices), 2), median(prices)
 
         flash(f"Search successful!\nThere are {count} results for '{keyword}', mean and median prices are {mean_price} € and {median_price} €, respectively.")
-        return render_template("home.html") #+ \
-            #'<script>document.getElementById("loading_header").style.display = "none"; document.getElementById("loading_bar").style.display = "none";</script>'
+        return render_template("home.html")
     else:
         return render_template("home.html")
+
 
 @app.route("/search_history", methods=["GET"])
 def search_history():
@@ -78,7 +71,7 @@ def search_history():
         history = rows.fetchall()
 
         # Notifies user in case where they did not conduct any search
-        if history == None:
+        if history is None:
             flash("Search history results are unavailable, you need to conduct a search first.")
             return render_template("search_history.html")
 
@@ -98,45 +91,45 @@ def search_history():
 
             results.append([search_keyword, time, count, round(mean, 2), med])
 
-        return render_template("search_history.html", results = results)
+        return render_template("search_history.html", results=results)
 
     flash("You must be logged in to access search history.")
     return redirect("/")
 
+
 @app.route("/register", methods=["GET", "POST"])
 def register():
-    
     if request.method == "POST":
         # Ensure user provides username
         if not request.form.get("username"):
-            return render_template("register.html", warning = generic_warning_message, username_error = True)
+            return render_template("register.html", warning=generic_warning_message, username_error=True)
 
         row = db.execute("SELECT * FROM users WHERE username = ?", (request.form.get("username"), ))
         check_username = row.fetchone()
 
         # Ensure username does not already exists and that passwords match
         if check_username:
-            return render_template("register.html", warning = f'Username "{check_username[1]}" is taken.', username_error = True)
+            return render_template("register.html", warning=f'Username "{check_username[1]}" is taken.', username_error=True)
         elif not request.form.get("password"):
-            return render_template("register.html", warning = generic_warning_message, password_error = True)
+            return render_template("register.html", warning=generic_warning_message, password_error=True)
         elif not request.form.get("confirmation"):
-            return render_template("register.html", warning = generic_warning_message, confirmation_error = True)
+            return render_template("register.html", warning=generic_warning_message, confirmation_error=True)
         elif not request.form.get("password") == request.form.get("confirmation"):
-            return render_template("register.html", warning = "Password do not match.", confirmation_error = True)
-        
+            return render_template("register.html", warning="Password do not match.", confirmation_error=True)
+
         username = request.form.get("username")
         password = request.form.get("password")
-        
+
         # Ensure password contains 6 characters including at least one uppercase character one lowercase character and one number
         numerical_characters = [str(x) for x in range(10)]
         if len(password) < 6:
-            return render_template("register.html", warning = "Password must contain 6 characters.", password_error = True)
+            return render_template("register.html", warning="Password must contain 6 characters.", password_error=True)
         elif not any(letter.isupper() for letter in password):
-            return render_template("register.html", warning = "Password does not contain uppercase character.", password_error = True)
+            return render_template("register.html", warning="Password does not contain uppercase character.", password_error=True)
         elif not any(letter.islower() for letter in password):
-            return render_template("register.html", warning = "Password does not contain lowercase character.", password_error = True)
+            return render_template("register.html", warning="Password does not contain lowercase character.", password_error=True)
         elif not any(number in password for number in numerical_characters):
-            return render_template("register.html", warning = "Password does not contain number.", password_error = True)
+            return render_template("register.html", warning="Password does not contain number.", password_error=True)
 
         # Hash user password and add username and password to the sql database
         hash_password = generate_password_hash(password)
@@ -148,6 +141,7 @@ def register():
 
     return render_template("register.html")
 
+
 @app.route("/login", methods=["GET", "POST"])
 def login():
 
@@ -158,9 +152,9 @@ def login():
     if request.method == "POST":
         # Ensure user provides username and password
         if not request.form.get("username"):
-            return render_template("login.html", warning = generic_warning_message, username_error = True)
+            return render_template("login.html", warning=generic_warning_message, username_error=True)
         elif not request.form.get("password"):
-            return render_template("login.html", warning = generic_warning_message, password_error = True)
+            return render_template("login.html", warning=generic_warning_message, password_error=True)
 
         row = db.execute("SELECT * FROM users WHERE username = ?", (request.form.get("username"), ))
         check_username_password = row.fetchone()
@@ -172,15 +166,15 @@ def login():
 
         # Check if username exists and password is correct
         if not check_username_password:
-            return render_template("login.html", warning = "Username does not exist.", username_error = True)
+            return render_template("login.html", warning="Username does not exist.", username_error=True)
         elif not check_password_hash(check_username_password[-1], request.form.get("password")):
-            return render_template("login.html", warning = "Wrong password.", password_error = True)
+            return render_template("login.html", warning="Wrong password.", password_error=True)
 
-        # Clear session if previous user was loged in and start new session 
+        # Clear session if previous user was loged in and start new session
         session.clear()
         session["id"] = check_username_password[0]
         session["user"] = check_username_password[1]
-        
+
         flash(f"Hi {check_username_password[1]}, you have been logged in.")
         return redirect("/")
     else:
@@ -200,6 +194,7 @@ def logout():
     # If the user is not loged in, redirect to current page
     flash("You are not logged in.")
     return redirect(request.referrer)
+
 
 @app.route("/about",  methods=["GET"])
 def about():
